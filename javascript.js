a58ac45d1a33a13d432688dac486f1c6010d88b7
Jean <154575672+Veuxirdev@users.noreply.github.com>
2025-05-30 17:31:19 -0400

    Do computerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,51 @@
-console.log("hello world")
+// Create values used in the game
+
+const paperVal = "Paper";
+const scissorsVal = "Scissors";
+const rockVal = "Rock";
+
+// Make computerChoice function
+
+function computerChoice(){
+
+    // Create a value that returns a number between 1 and 3
+
+    const randomVal = Math.floor(Math.random() * 3) + 1;
+
+    // if randomVal is 1 return paper, if 2 return Scissors, if 3 return Rock
+ 
+    if(randomVal === 1){
+        return paperVal;
+    } else if (randomVal === 2){
+        return scissorsVal;
+    } else if (randomVal ===  3){
+        return rockVal;
+    } 
+
+    // in one line
+ 
+    /*     
+    return randomVal === 1? paperVal : randomVal === 2? scissorsVal : randomVal === 3? rockVal : console.error("no value");
+     */
+
+
+    // with switch
+
+/* 
+    switch (randomVal) {
+        case 1:
+            return paperVal;
+    
+        case 2:
+            return scissorsVal;
+        
+        case 3:
+            return rockVal;
+        
+        default:
+            break;
+    }
+     */
+}
+
+computerChoice();
